75f3c580cf48c6f27ddb3fd8a179739cab2ff98b
iiafn <afnan_twirqi@hotmail.com>
2024-05-27 14:47:13 +0300

    add HW

---
+++ main.js
@@ -0,0 +1,114 @@
+// let name1 = document.getElementById('name');
+// function changeName() {
+//     if (name1.innerText === 'RICHARD SANCHEZ') {
+//         name1.innerText = 'AFNAN ALTOWAIRQI';
+//     } else {
+//         name1.innerText = 'RICHARD SANCHEZ';
+//     }
+// }
+// name1.onmouseover = changeName;
+// name1.onmouseout = changeName;
+
+// بدون ما اضيف النص في ال html
+// let specialty = document.querySelector('.specialty');
+// function changeIT() {
+//     if (specialty.innerText === 'MARKETING MANAGER') {
+//         specialty.innerText = 'INFORMATION TECHNOLOGY';
+//     } else {
+//         specialty.innerText = 'MARKETING MANAGER';
+//     }
+// }
+// specialty.onmouseover = changeIT;
+// specialty.onmouseout = changeIT;
+
+//change Avatar
+function changeAvatar(){
+    document.getElementById('avatar1').classList.toggle('hidden');
+    document.getElementById('avatar2').classList.toggle('hidden');
+}
+avatar1.onmouseover = changeAvatar;
+avatar2.onmouseout = changeAvatar;
+//change Name
+function changeName(){
+    document.getElementById('name1').classList.toggle('hidden');
+    document.getElementById('name2').classList.toggle('hidden');
+}
+name1.onmouseover = changeName;
+name2.onmouseout = changeName;
+//change specialty
+function changeIT(){
+    document.getElementById('specialty1').classList.toggle('hidden');
+    document.getElementById('specialty2').classList.toggle('hidden');
+}
+specialty1.onmouseover = changeIT;
+specialty2.onmouseout = changeIT;
+//change profile text
+function profileText() {
+    document.getElementById('profile1').classList.toggle('hidden');
+    document.getElementById('profile2').classList.toggle('hidden');
+}
+profile1.onmouseover = profileText;
+profile2.onmouseout = profileText;
+//change skills
+function changeSkills() {
+    document.getElementById('skills1').classList.toggle('hidden');
+    document.getElementById('skills2').classList.toggle('hidden');
+}
+skills1.onmouseover = changeSkills;
+skills2.onmouseout = changeSkills;
+////change languages
+function changeLang() {
+    document.getElementById('Lang1').classList.toggle('hidden');
+    document.getElementById('Lang2').classList.toggle('hidden');
+}
+Lang1.onmouseover = changeLang;
+Lang2.onmouseout = changeLang;
+//change Education
+function changeEdu() {
+    document.getElementById('edu1').classList.toggle('hidden');
+    document.getElementById('edu2').classList.toggle('hidden');
+}
+edu1.onmouseover = changeEdu;
+edu2.onmouseout = changeEdu;
+//change Contact
+function changeCont() {
+    document.getElementById('contact1').classList.toggle('hidden');
+    document.getElementById('contact2').classList.toggle('hidden');
+}
+contact1.onmouseover = changeCont;
+contact2.onmouseout = changeCont;
+//change experince
+function changeTextExp() {
+    document.getElementById('expText1').classList.toggle('hidden');
+    document.getElementById('expText2').classList.toggle('hidden');
+}
+expText1.onmouseover = changeTextExp;
+expText2.onmouseout = changeTextExp;
+// change title exp1
+function changeTitleExp1() {
+    document.getElementById('titleEx1').classList.toggle('hidden');
+    document.getElementById('titleEx2').classList.toggle('hidden');
+}
+titleEx1.onmouseover = changeTitleExp1;
+titleEx2.onmouseout = changeTitleExp1;
+//change experince2
+function changeTextExp2() {
+    document.getElementById('expText3').classList.toggle('hidden');
+    document.getElementById('expText4').classList.toggle('hidden');
+}
+expText3.onmouseover = changeTextExp2;
+expText4.onmouseout = changeTextExp2;
+//chage title exp2
+function changeTitleExp2() {
+    document.getElementById('titleExp3').classList.toggle('hidden');
+    document.getElementById('titleExp4').classList.toggle('hidden');
+}
+titleExp3.onmouseover = changeTitleExp2;
+titleExp4.onmouseout = changeTitleExp2;
+//change peference
+function changePer() {
+    document.getElementById('pefer1').classList.toggle('hidden');
+    document.getElementById('pefer2').classList.toggle('hidden');
+}
+pefer1.onmouseover = changePer;
+pefer2.onmouseout = changePer;
